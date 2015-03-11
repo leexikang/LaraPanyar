aa25637f5a3f9110b9e4c5d239c82958712dacac
leexikang <leexikang@gmail.com>
2015-03-11 14:41:46 +0630

    Chage user login design

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,47 +1,51 @@
 @include("layouts.partials.header")
 @include("layouts.partials.nav")
 <div class='container'>
-<div class='row'>
+<br/><br/><br/>
+	<div class='row'>
+		<div class="col-md-5 col-md-offset-3">
+
+			{{ Form::open(array('route' => 'session.store',
+				'method' => 'POST'
+				)) }}
+
+				<div class='form-group'>
+
+					{{ Form::label('name', 'Username:')}}
+					{{ Form::text('name', null, array('class' => 'form-control') ) }}
+
+				</div>
+
+				<div class='form-group'>
+
+					{{ Form::label('password', 'Password')}}
+					{{ Form::password('password', array('class' => 'form-control') ) }}
+				</div>
+				<div class="form-group">
+
+					{{ Form::submit('Login', array('class' => 'btn btn-block btn-lg btn-login') )}}
+
+				</div>
+
+				<br/><br/>
 
 
-	{{ Form::open(array('route' => 'session.store',
-						'method' => 'POST'
-						)) }}
+				{{ Form::close() }}
+				@if ( $error = Session::get('flash_message') )
 
-	<div class='form-group'>
+				<div class="alert alert-danger" role="alert">
+					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+					<span class="sr-only">Error:</span>
+					{{ $error }}
+				</div>
 
-		{{ Form::label('name', 'Username:')}}
-		{{ Form::text('name', null, array('class' => 'form-control') ) }}
-
+				@endif
+			</div>
+		</div>
 	</div>
 
-	<div class='form-group'>
-
-		{{ Form::label('password', 'Password')}}
-		{{ Form::password('password', array('class' => 'form-control') ) }}
-	</div>
-	{{ Form::submit()}}
-
-	<br/><br/>
 
 
-	{{ Form::close() }}
-	@if ( $error = Session::get('flash_message') )
 
-	<div class="alert alert-danger" role="alert">
- 	<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
- 	<span class="sr-only">Error:</span>
-	{{ $error }}
-	</div>
 
-	@endif
-
-</div>
-</div>
-
-@include("layouts.partials.footer")
-							
-
-			
-				
 	
