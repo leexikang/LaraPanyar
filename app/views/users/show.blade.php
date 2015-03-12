683888bf8ac6c7af4a32a68c77638f8a69a8f95c
leexikang <leexikang@gmail.com>
2015-03-12 16:25:24 +0630

    create user show page

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,5 +1,21 @@
-@extends('layouts.user')
+@extends('layouts.master')
 
 @section('content')
+
+	<div class='container'>
+	<div class='row'>
+
+
+		<div class='col-md-7'>
+		<h1> {{ $user->name }} </h1>
+		<h3> About the Course </h3>
+		<p class="text-justify"> {{ $user->intro }} </p>
+	<address>
+	<strong> {{ $user->name }}</strong><br/>
+	{{ $user->address}}	<br/>
+	{{$user->email }}
+	</address>
+
+	{{ link_to("/", "Back", ["class" => "btn btn-panyar"])}}
 
 @stop
